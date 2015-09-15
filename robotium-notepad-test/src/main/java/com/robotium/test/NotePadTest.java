1d0249e13aa16a74ec0a57d27b5b84fa70bb23b2
wangjian.deng <wangjian.deng@tcl.com>
2015-09-15 16:09:25 +0800

    填加UiAutomator，执行编程Robotium+UiAutomator混合执行方式

--- robotium-notepad-test/src/main/java/com/robotium/test/NotePadTest.java
+++ robotium-notepad-test/src/main/java/com/robotium/test/NotePadTest.java
@@ -12,103 +12,104 @@
 
 package com.robotium.test;
 
-import com.robotium.solo.Solo;
+import android.app.Instrumentation;
+import android.support.test.uiautomator.UiDevice;
 import android.test.ActivityInstrumentationTestCase2;
 
-public class NotePadTest extends ActivityInstrumentationTestCase2{
-	private static final String MAIN_ACTIVITY_NAME 	= "com.example.android.notepad.NotesList" ;
+import com.robotium.solo.TSolo;
+import com.robotium.solo.tcl.runner.TActivityInstrumentationTestCase2;
 
-	private Solo solo;
-	
-	private static Class<?> activityClass  ;
 
-	static {
-		try {
-			activityClass = Class.forName(MAIN_ACTIVITY_NAME) ;
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("MainActivity class load has failed...") ;
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	public NotePadTest() {
-		super(activityClass);
-	}
+public class NotePadTest extends TActivityInstrumentationTestCase2 {
 
-	@Override
-	public void setUp() throws Exception {
-		//setUp() is run before a test case is started. 
-		//This is where the solo object is created.
-		solo = new Solo(getInstrumentation(), getActivity());
-	}
+    private static final String MAIN_ACTIVITY_NAME 	= "com.example.android.notepad.NotesList" ;
 
-	public void testAddNote() throws Exception {
-		//Unlock the lock screen
-		solo.unlockScreen();
-		solo.clickOnMenuItem("Add note");
+    private TSolo solo;
+
+    private static Class<?> activityClass  ;
+
+    private Instrumentation instrumentation ;
+
+    private UiDevice mDevice;
+
+    static {
+        try {
+            activityClass = Class.forName(MAIN_ACTIVITY_NAME) ;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MainActivity class load has failed...") ;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public NotePadTest() {
+        super(activityClass);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        //setUp() is run before a test case is started.
+        //This is where the solo object is created.
+        instrumentation = getInstrumentation() ;
+//        solo = new Solo(instrumentation , getActivity()) ;
+        solo = new TSolo(instrumentation , getActivity()) ;
+        setSolo(solo) ;
+        mDevice = UiDevice.getInstance(instrumentation);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        //tearDown() is run after a test case has finished.
+        //finishOpenedActivities() will finish all the activities that have been opened during the test execution.
+        solo.finishOpenedActivities();
+    }
+
+    public void testAddNote() throws Exception {
+        //Robotium code : add a new note
+
+        //Unlock the lock screen
+        solo.unlockScreen();
+        solo.clickOnMenuItem("Add note");
 		//Assert that NoteEditor activity is opened
 		solo.assertCurrentActivity("Expected NoteEditor activity", "NoteEditor"); 
 		//In text field 0, enter Note 1
 		solo.enterText(0, "Note 1");
 		solo.goBack(); 
-		//Clicks on menu item
-		solo.clickOnMenuItem("Add note");
-		//In text field 0, type Note 2
-		solo.typeText(0, "Note 2");
-		//Go back to first activity
-		solo.goBack(); 
+
 		//Takes a screenshot and saves it in "/sdcard/Robotium-Screenshots/".
 		solo.takeScreenshot();
-		boolean notesFound = solo.searchText("Note 1") && solo.searchText("Note 2");
+		boolean notesFound = solo.searchText("Note 1") ;
 		//Assert that Note 1 & Note 2 are found
-		assertTrue("Note 1 and/or Note 2 are not found", notesFound); 
+		assertTrue("Note 1 are not found", notesFound);
 
-	}
+        //UiAutomator Code :
 
-	public void testEditNote() throws Exception {
-		// Click on the second list line
-		solo.clickInList(2); 
-		//Hides the soft keyboard
-		solo.hideSoftKeyboard();
-		// Change orientation of activity
-		solo.setActivityOrientation(Solo.LANDSCAPE);
-		// Change title
-		solo.clickOnMenuItem("Edit title");
-		//In first text field (0), add test
-		solo.enterText(0, " test");  
-		solo.goBack();
-		solo.setActivityOrientation(Solo.PORTRAIT);
-		// (Regexp) case insensitive
-		boolean noteFound = solo.waitForText("(?i).*?note 1 test"); 
-		//Assert that Note 1 test is found
-		assertTrue("Note 1 test is not found", noteFound); 
+        solo.sleep(5000) ;
+        //use UiDevice to open notification
+        System.out.println("viking flag ------------------open notification");
+        mDevice.openNotification() ;
 
-	}
-	
-	public void testRemoveNote() throws Exception {
-		//(Regexp) case insensitive/text that contains "test"
-		solo.clickOnText("(?i).*?test.*");
-		//Delete Note 1 test
-		solo.clickOnMenuItem("Delete");
-		//Note 1 test should not be found
-		boolean noteFound = solo.searchText("Note 1 test");
-		//Assert that Note 1 test is not found
-		assertFalse("Note 1 Test is found", noteFound);  
-		solo.clickLongOnText("Note 2");
-		//Clicks on Delete in the context menu
-		solo.clickOnText("Delete");  
-		//Will wait 100 milliseconds for the text: "Note 2"
-		noteFound = solo.waitForText("Note 2", 1, 100);
-		//Assert that Note 2 is not found
-		assertFalse("Note 2 is found", noteFound);  
-	}
+        //use UiDevice to go back
+        System.out.println("viking flag ------------------go back");
+        solo.sleep(3000);
+        mDevice.pressBack() ;
+        solo.sleep(3000);
 
-    @Override
-    public void tearDown() throws Exception {
-        //tearDown() is run after a test case has finished.
-        //finishOpenedActivities() will finish all the activities
-        // that have been opened during the test execution.
-        solo.finishOpenedActivities();
+        //use UiDevice get current package name
+        if(mDevice != null){
+            System.out.println("viking flag ---------print package name : " + mDevice.getCurrentPackageName());
+        }
+        solo.sleep(5000) ;
+
+        //Robotium Code : create a new note
+        solo.clickOnMenuItem("Add note");
+        //Assert that NoteEditor activity is opened
+        solo.assertCurrentActivity("Expected NoteEditor activity", "NoteEditor");
+        //In text field 0, enter Note 1
+        solo.enterText(0, "Note 2");
+        solo.goBack();
+        //Assert that Note 1 & Note 2 are found
+        solo.sleep(2000);
+        assertTrue("Note 2 are not found", solo.searchText("Note 2"));
+
     }
-
 }
